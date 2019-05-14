42898dbf5cd0cdba82eef45bbadc11d5dc3573c9
codemilli <hckrmoon@gmail.com>
2019-05-14 21:57:59 +0900

    feat: add lezhin logo

--- sponsor/src/js/components/sections/DueSection.ts
+++ sponsor/src/js/components/sections/DueSection.ts
@@ -4,7 +4,7 @@ export default class DueSection extends Section {
   render() {
     return `
       <h1 class="ft-title">후원사 모집 기간</h1>
-      <h1 class="ft-sub">2019년 5월 13일 부터 2019년 6월 30일 까지</h1>
+      <h1 class="ft-sub">2019년 5월 15일 부터 2019년 6월 30일 까지</h1>
     `;
   }
 }
