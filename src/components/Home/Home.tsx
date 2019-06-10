9958351e141e026bd3d8e97c639b282f5062a5b9
codemilli <hckrmoon@gmail.com>
2019-06-10 15:35:34 +0900

    feat: redireciton

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Head from 'next/head';
 import css from './Home.scss';
 
 interface Props {}
@@ -6,7 +7,9 @@ interface Props {}
 const Home: React.FC<Props> = () => {
   return (
     <div className={css.Home}>
-
+      <Head>
+        <meta http-equiv="refresh" content="0; url=https://2019.feconf.kr/cfs"/>
+      </Head>
     </div>
   );
 }
