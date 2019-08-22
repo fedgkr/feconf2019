e008db5df71f7b675226613a6eed5dc3bb7fdde8
codemilli <hckrmoon@gmail.com>
2019-08-22 16:25:13 +0900

    feat: add Main Page

--- src/views/containers/Home/Home.tsx
+++ src/views/containers/Home/Home.tsx
@@ -6,7 +6,17 @@ interface Props {}
 const Home: React.FC<Props> = () => {
   return (
     <div className={css.Home}>
+      <div className={css.TextWrap}>
+        <div className={css.Title}>
+          <h3>FE Conference 2019</h3>
+          <h3>is for</h3>
+          <h2>Sustainability</h2>
+        </div>
 
+      </div>
+      <div className={css.ImageWrap}>
+
+      </div>
     </div>
   );
 }
