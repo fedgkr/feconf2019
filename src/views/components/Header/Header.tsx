b4c057e24cfb136d9ff6f42128921ab94d6b5279
codemilli <hckrmoon@gmail.com>
2019-08-22 18:43:51 +0900

    feat: add Link component

--- src/views/components/Header/Header.tsx
+++ src/views/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 import css from './Header.scss';
 
 interface HeaderProps {
@@ -7,13 +8,15 @@ interface HeaderProps {
 const Header: React.FC<HeaderProps> = () => {
   return (
     <div className={css.Header}>
-      <img src="/static/images/logo@2x.png" alt="FEConf"/>
+      <Link href="/">
+        <a><img src="/static/images/logo@2x.png" alt="FEConf"/></a>
+      </Link>
       <ul className={css.Menu}>
-        <li><a href="/about">About FE 2019</a></li>
-        <li><a href="/speakers">Speakers</a></li>
-        <li><a href="/schedule">Schedule</a></li>
-        <li><a href="/sponsors">Sponsors</a></li>
-        <li><a href="/organizers">Organizers</a></li>
+        <li><Link href="/about"><a>About FE 2019</a></Link></li>
+        <li><Link href="/speakers"><a>Speakers</a></Link></li>
+        <li><Link href="/schedule"><a>Schedule</a></Link></li>
+        <li><Link href="/sponsors"><a>Sponsors</a></Link></li>
+        <li><Link href="/organizers"><a>Organizers</a></Link></li>
         <a href="/apply"><button>신청하기</button></a>
       </ul>
     </div>
