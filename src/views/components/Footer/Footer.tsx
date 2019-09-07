16310591959b6dcbfa80fa77937318e515bc52f3
codemilli <hckrmoon@gmail.com>
2019-09-07 20:36:29 +0900

    feat: remove footer links

--- src/views/components/Footer/Footer.tsx
+++ src/views/components/Footer/Footer.tsx
@@ -19,11 +19,11 @@ const Footer: React.FC<FooterProps> = () => {
           </div>
         </div>
         <div className={css.Info}>
-          <div className={css.Links}>
-            <Link href="/organizers"><a>FEConf 2019 Organizers</a></Link>
-            <span> / </span>
-            <Link href="/coc"><a>Our Code of Conduct</a></Link>
-          </div>
+          {/*<div className={css.Links}>*/}
+          {/*  <Link href="/organizers"><a>FEConf 2019 Organizers</a></Link>*/}
+          {/*  <span> / </span>*/}
+          {/*  <Link href="/coc"><a>Our Code of Conduct</a></Link>*/}
+          {/*</div>*/}
         </div>
       </div>
     </div>
