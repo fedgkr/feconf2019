6f1f7b4b18de2a7b5d613003bc460ebecb185633
codemilli <hckrmoon@gmail.com>
2019-09-23 23:58:19 +0900

    feat: add sign

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -18,6 +18,7 @@ export default class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          /** Thanks to Ko */
         </body>
       </html>
     );
